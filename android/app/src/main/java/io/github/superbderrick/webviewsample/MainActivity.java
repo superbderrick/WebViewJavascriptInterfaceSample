21d89c67df5e3178e738016e12807645a78e9ddc
derrick <kang.derrick@gmail.com>
2018-06-10 17:13:43 +0900

    added more setting proferties for web

--- android/app/src/main/java/io/github/superbderrick/webviewsample/MainActivity.java
+++ android/app/src/main/java/io/github/superbderrick/webviewsample/MainActivity.java
@@ -61,10 +61,28 @@ public class MainActivity extends AppCompatActivity  {
         }
 
 
-
-
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setDefaultTextEncodingName("UTF-8");
+
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        // 화면에 맞게 WebView 사이즈를 정의
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        // ViewPort meta tag를 활성화 여부
+        mWebView.getSettings().setUseWideViewPort(true);
+        // 줌 컨트롤 사용 여부
+        mWebView.getSettings().setDisplayZoomControls(false);
+        // 사용자 제스처를 통한 줌 기능 활성화 여부
+        mWebView.getSettings().setSupportZoom(false);
+        // TextEncoding 이름 정의
+
+        // Setting Local Storage
+        mWebView.getSettings().setDatabaseEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+
+        // 캐쉬 사용 방법을 정의
+        mWebView.getSettings().setCacheMode(mWebView.getSettings().LOAD_NO_CACHE);
+
+
         mWebView.addJavascriptInterface(mJavascriptInterface , "interface");
         mWebView.loadUrl(HTML_PATH);
         mWebView.setWebViewClient(new WebViewClient() {
